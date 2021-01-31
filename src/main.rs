b2f70aec6cc68842dc97f756b725092974afc78c
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-02-01 08:12:42 +0900

    read integer (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut integer = String::new();
+
+    io::stdin()
+        .read_line(&mut integer)
+        .expect("Failed to read line.");
+
+    let integer: u32 = integer.trim().parse().unwrap();
+
+    println!("(module");
+    println!("  (func (result i32)");
+    println!("    (i32.const {})", integer);
+    println!("  )");
+    println!("  (export \"main\" (func 0))");
+    println!(")");
 }
