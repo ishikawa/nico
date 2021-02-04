0b41cfa64db3a37ed26184688b92117da2f679d5
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-02-05 08:19:48 +0900

    add assert_matches

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,5 @@
 use std::{iter::Peekable, panic, str::Chars};
-
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub enum Node {
     Integer(u32),
     Add(Box<Node>, Box<Node>),
@@ -132,26 +131,38 @@ fn next_operator(src: &mut Peekable<Chars>) -> Option<char> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use assert_matches::assert_matches;
+
     #[test]
     fn number_integer() {
         let node = parse("42").unwrap();
-        assert_eq!(*node, Node::Integer(42));
+        assert_matches!(*node, Node::Integer(42));
     }
     #[test]
     fn number_integer_followed_by_letter() {
         let node = parse("123a").unwrap();
-        assert_eq!(*node, Node::Integer(123));
+        assert_matches!(*node, Node::Integer(123));
     }
 
     #[test]
     fn add_integer() {
         let node = parse("1 + 2").unwrap();
-        match *node {
-            Node::Add(lhs, rhs) => {
-                assert_eq!(*lhs, Node::Integer(1));
-                assert_eq!(*rhs, Node::Integer(2));
-            }
-            _ => panic!(),
-        }
+        assert_matches!(*node, Node::Add(lhs, rhs) => {
+            assert_matches!(*lhs, Node::Integer(1));
+            assert_matches!(*rhs, Node::Integer(2));
+        });
+    }
+
+    #[test]
+    fn operator_associative() {
+        let node = parse("1 + 2 + 3").unwrap();
+
+        assert_matches!(*node, Node::Add(lhs, rhs) => {
+            assert_matches!(*lhs, Node::Add(x, y) => {
+                assert_matches!(*x, Node::Integer(1));
+                assert_matches!(*y, Node::Integer(2));
+            });
+            assert_matches!(*rhs, Node::Integer(3));
+        });
     }
 }
