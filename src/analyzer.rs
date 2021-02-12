90761d44de6835b653dc2b19dfa7f670189a18b3
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-02-12 10:18:07 +0900

    self

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -8,169 +8,7 @@ use std::rc::Rc;
 pub struct Semantic {
     // Type environment
     environment: HashMap<String, Rc<sem::Type>>,
-    non_generic: HashMap<String, Rc<sem::Type>>,
-    type_variable_index: i32,
-}
-
-fn new_typevar() -> sem::Type {
-    let i = 0;
-
-    sem::Type::TypeVariable {
-        name: format!("{}", i),
-        instance: None,
-    }
-}
-
-/// Type operator and generic variables are duplicated; non-generic variables are shared.
-fn fresh(
-    ty: &Rc<RefCell<sem::Type>>,
-    non_generic_vars: &mut HashSet<String>,
-) -> Rc<RefCell<sem::Type>> {
-    let mut mappings = HashMap::new();
-    freshrec(ty, non_generic_vars, &mut mappings)
-}
-
-fn freshrec(
-    ty: &Rc<RefCell<sem::Type>>,
-    non_generic_vars: &mut HashSet<String>,
-    mappings: &mut HashMap<String, Rc<RefCell<sem::Type>>>,
-) -> Rc<RefCell<sem::Type>> {
-    let ty = prune(ty);
-
-    let freshed = match *ty.borrow() {
-        sem::Type::TypeVariable { ref name, .. } => {
-            if non_generic_vars.contains(name) {
-                return Rc::clone(&ty);
-            } else {
-                if let Some(cached) = mappings.get(name) {
-                    return Rc::clone(cached);
-                } else {
-                    let cached = sem::Type::TypeVariable {
-                        name: "a".to_string(),
-                        instance: None,
-                    };
-                    let cached = Rc::new(RefCell::new(cached));
-
-                    mappings.insert(name.clone(), Rc::clone(&cached));
-                    return cached;
-                }
-            }
-        }
-        // Type operators
-        sem::Type::Int32 => sem::Type::Int32,
-        sem::Type::Boolean => sem::Type::Boolean,
-        sem::Type::String => sem::Type::String,
-        sem::Type::Function {
-            ref params,
-            ref return_type,
-        } => {
-            let params = params
-                .iter()
-                .map(|x| freshrec(x, non_generic_vars, mappings))
-                .collect();
-            let return_type = freshrec(&return_type, non_generic_vars, mappings);
-
-            sem::Type::Function {
-                params,
-                return_type,
-            }
-        }
-    };
-
-    Rc::new(RefCell::new(freshed))
-}
-
-fn prune(ty: &Rc<RefCell<sem::Type>>) -> Rc<RefCell<sem::Type>> {
-    match *ty.borrow_mut() {
-        sem::Type::TypeVariable {
-            instance: Some(ref mut instance),
-            ..
-        } => {
-            *instance = prune(instance);
-            Rc::clone(&instance)
-        }
-        _ => Rc::clone(ty),
-    }
-}
-
-fn unify(ty1: &Rc<RefCell<sem::Type>>, ty2: &Rc<RefCell<sem::Type>>) {
-    let ty1 = prune(ty1);
-    let ty2 = prune(ty2);
-
-    let new_instance = match *ty1.borrow() {
-        sem::Type::TypeVariable { .. } => {
-            if *ty1 != *ty2 {
-                if (*ty1).borrow().contains(&*ty2.borrow()) {
-                    panic!("recursive unification");
-                }
-                Some(&ty2)
-            } else {
-                None
-            }
-        }
-        sem::Type::Int32 => match *ty2.borrow() {
-            sem::Type::Int32 => None,
-            sem::Type::TypeVariable { .. } => {
-                unify(&ty2, &ty1);
-                None
-            }
-            _ => {
-                panic!("type error: {:?}", *ty1);
-            }
-        },
-        sem::Type::Boolean => match *ty2.borrow() {
-            sem::Type::Boolean => None,
-            sem::Type::TypeVariable { .. } => {
-                unify(&ty2, &ty1);
-                None
-            }
-            _ => panic!("type error: {:?}", *ty1),
-        },
-        sem::Type::String => match *ty2.borrow() {
-            sem::Type::String => None,
-            sem::Type::TypeVariable { .. } => {
-                unify(&ty2, &ty1);
-                None
-            }
-            _ => panic!("type error: {:?}", *ty1),
-        },
-        sem::Type::Function {
-            params: ref params1,
-            return_type: ref return_type1,
-        } => match *ty2.borrow() {
-            sem::Type::Function {
-                params: ref params2,
-                return_type: ref return_type2,
-            } => {
-                if params1.len() != params2.len() {
-                    panic!("The number of params differs: {:?}", *ty1);
-                }
-
-                for (x, y) in params1.iter().zip(params2.iter()) {
-                    unify(x, y);
-                }
-
-                unify(return_type1, return_type2);
-                None
-            }
-            sem::Type::TypeVariable { .. } => {
-                unify(&ty2, &ty1);
-                None
-            }
-            _ => panic!("type error: {:?}", *ty1),
-        },
-    };
-
-    if let Some(new_instance) = new_instance {
-        match *ty1.borrow_mut() {
-            sem::Type::TypeVariable {
-                ref mut instance, ..
-            } => {
-                instance.replace(Rc::clone(new_instance));
-            }
-            _ => {}
-        }
-    }
+    type_var_index: i32,
 }
 
 #[allow(unused_variables)]
@@ -178,8 +16,7 @@ impl Semantic {
     pub fn new() -> Semantic {
         Semantic {
             environment: HashMap::new(),
-            non_generic: HashMap::new(),
-            type_variable_index: 0,
+            type_var_index: 0,
         }
     }
 
@@ -195,25 +32,13 @@ impl Semantic {
     }
 
     fn new_type_variable(&mut self) -> Rc<sem::Type> {
-        self.type_variable_index += 1;
+        self.type_var_index += 1;
 
         let ty = sem::Type::TypeVariable {
-            name: format!("${}", self.type_variable_index),
+            name: format!("${}", self.type_var_index),
             instance: None,
         };
         Rc::new(ty)
-    }
-
-    fn fresh(&self, ty: &sem::Type) -> Option<sem::Type> {
-        None
-    }
-
-    fn lookup(&self, name: &str) -> Option<sem::Type> {
-        if let Some(ty) = self.environment.get(name) {
-            self.fresh(ty)
-        } else {
-            None
-        }
     }
 
     fn analyze_function(&mut self, function: &mut parser::Function) {
@@ -275,6 +100,170 @@ impl Semantic {
     }
 }
 
+impl Semantic {
+    fn new_type_var(&mut self) -> sem::Type {
+        let i = self.type_var_index;
+
+        self.type_var_index += 1;
+
+        sem::Type::TypeVariable {
+            name: format!("{}", i),
+            instance: None,
+        }
+    }
+
+    /// Type operator and generic variables are duplicated; non-generic variables are shared.
+    fn fresh(
+        &mut self,
+        ty: &Rc<RefCell<sem::Type>>,
+        non_generic_vars: &mut HashSet<String>,
+    ) -> Rc<RefCell<sem::Type>> {
+        let mut mappings = HashMap::new();
+        self.freshrec(ty, non_generic_vars, &mut mappings)
+    }
+
+    fn freshrec(
+        &mut self,
+        ty: &Rc<RefCell<sem::Type>>,
+        non_generic_vars: &mut HashSet<String>,
+        mappings: &mut HashMap<String, Rc<RefCell<sem::Type>>>,
+    ) -> Rc<RefCell<sem::Type>> {
+        let ty = self.prune(ty);
+
+        let freshed = match *ty.borrow() {
+            sem::Type::TypeVariable { ref name, .. } => {
+                if non_generic_vars.contains(name) {
+                    return Rc::clone(&ty);
+                } else {
+                    if let Some(cached) = mappings.get(name) {
+                        return Rc::clone(cached);
+                    } else {
+                        let cached = self.new_type_var();
+                        let cached = Rc::new(RefCell::new(cached));
+
+                        mappings.insert(name.clone(), Rc::clone(&cached));
+                        return cached;
+                    }
+                }
+            }
+            // Type operators
+            sem::Type::Int32 => sem::Type::Int32,
+            sem::Type::Boolean => sem::Type::Boolean,
+            sem::Type::String => sem::Type::String,
+            sem::Type::Function {
+                ref params,
+                ref return_type,
+            } => {
+                let params = params
+                    .iter()
+                    .map(|x| self.freshrec(x, non_generic_vars, mappings))
+                    .collect();
+                let return_type = self.freshrec(&return_type, non_generic_vars, mappings);
+
+                sem::Type::Function {
+                    params,
+                    return_type,
+                }
+            }
+        };
+
+        Rc::new(RefCell::new(freshed))
+    }
+
+    fn prune(&mut self, ty: &Rc<RefCell<sem::Type>>) -> Rc<RefCell<sem::Type>> {
+        match *ty.borrow_mut() {
+            sem::Type::TypeVariable {
+                instance: Some(ref mut instance),
+                ..
+            } => {
+                *instance = self.prune(instance);
+                Rc::clone(&instance)
+            }
+            _ => Rc::clone(ty),
+        }
+    }
+
+    fn unify(&mut self, ty1: &Rc<RefCell<sem::Type>>, ty2: &Rc<RefCell<sem::Type>>) {
+        let ty1 = self.prune(ty1);
+        let ty2 = self.prune(ty2);
+
+        let new_instance = match *ty1.borrow() {
+            sem::Type::TypeVariable { .. } => {
+                if *ty1 != *ty2 {
+                    if (*ty1).borrow().contains(&*ty2.borrow()) {
+                        panic!("recursive unification");
+                    }
+                    Some(&ty2)
+                } else {
+                    None
+                }
+            }
+            sem::Type::Int32 => match *ty2.borrow() {
+                sem::Type::Int32 => None,
+                sem::Type::TypeVariable { .. } => {
+                    self.unify(&ty2, &ty1);
+                    None
+                }
+                _ => {
+                    panic!("type error: {:?}", *ty1);
+                }
+            },
+            sem::Type::Boolean => match *ty2.borrow() {
+                sem::Type::Boolean => None,
+                sem::Type::TypeVariable { .. } => {
+                    self.unify(&ty2, &ty1);
+                    None
+                }
+                _ => panic!("type error: {:?}", *ty1),
+            },
+            sem::Type::String => match *ty2.borrow() {
+                sem::Type::String => None,
+                sem::Type::TypeVariable { .. } => {
+                    self.unify(&ty2, &ty1);
+                    None
+                }
+                _ => panic!("type error: {:?}", *ty1),
+            },
+            sem::Type::Function {
+                params: ref params1,
+                return_type: ref return_type1,
+            } => match *ty2.borrow() {
+                sem::Type::Function {
+                    params: ref params2,
+                    return_type: ref return_type2,
+                } => {
+                    if params1.len() != params2.len() {
+                        panic!("The number of params differs: {:?}", *ty1);
+                    }
+
+                    for (x, y) in params1.iter().zip(params2.iter()) {
+                        self.unify(x, y);
+                    }
+
+                    self.unify(return_type1, return_type2);
+                    None
+                }
+                sem::Type::TypeVariable { .. } => {
+                    self.unify(&ty2, &ty1);
+                    None
+                }
+                _ => panic!("type error: {:?}", *ty1),
+            },
+        };
+
+        if let Some(new_instance) = new_instance {
+            match *ty1.borrow_mut() {
+                sem::Type::TypeVariable {
+                    ref mut instance, ..
+                } => {
+                    instance.replace(Rc::clone(new_instance));
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
 impl Default for Semantic {
     fn default() -> Self {
         Semantic::new()
@@ -296,13 +285,15 @@ mod tests {
 
     #[test]
     fn prune_0() {
+        let mut semantic = Semantic::new();
+
         let ty0 = sem::Type::TypeVariable {
             name: "$1".to_string(),
             instance: None,
         };
 
         let pty0 = Rc::new(RefCell::new(ty0));
-        let pty1 = prune(&pty0);
+        let pty1 = semantic.prune(&pty0);
 
         assert_matches!(*pty0.borrow(), sem::Type::TypeVariable {
             ref name,
@@ -322,13 +313,14 @@ mod tests {
 
     #[test]
     fn prune_1() {
+        let mut semantic = Semantic::new();
         let ty0 = sem::Type::TypeVariable {
             name: "$1".to_string(),
             instance: Some(Rc::new(RefCell::new(sem::Type::Int32))),
         };
 
         let pty0 = Rc::new(RefCell::new(ty0));
-        let pty1 = prune(&pty0);
+        let pty1 = semantic.prune(&pty0);
 
         assert_matches!(*pty0.borrow(), sem::Type::TypeVariable {
             ref name,
@@ -342,30 +334,33 @@ mod tests {
 
     #[test]
     fn unify_int32() {
+        let mut semantic = Semantic::new();
         let pty0 = Rc::new(RefCell::new(sem::Type::Int32));
         let pty1 = Rc::new(RefCell::new(sem::Type::Int32));
 
-        unify(&pty0, &pty1);
+        semantic.unify(&pty0, &pty1);
 
         assert_matches!(*pty0.borrow(), sem::Type::Int32);
         assert_matches!(*pty1.borrow(), sem::Type::Int32);
     }
     #[test]
     fn unify_boolean() {
+        let mut semantic = Semantic::new();
         let pty0 = Rc::new(RefCell::new(sem::Type::Boolean));
         let pty1 = Rc::new(RefCell::new(sem::Type::Boolean));
 
-        unify(&pty0, &pty1);
+        semantic.unify(&pty0, &pty1);
 
         assert_matches!(*pty0.borrow(), sem::Type::Boolean);
         assert_matches!(*pty1.borrow(), sem::Type::Boolean);
     }
     #[test]
     fn unify_string() {
+        let mut semantic = Semantic::new();
         let pty0 = Rc::new(RefCell::new(sem::Type::String));
         let pty1 = Rc::new(RefCell::new(sem::Type::String));
 
-        unify(&pty0, &pty1);
+        semantic.unify(&pty0, &pty1);
 
         assert_matches!(*pty0.borrow(), sem::Type::String);
         assert_matches!(*pty1.borrow(), sem::Type::String);
@@ -373,6 +368,7 @@ mod tests {
 
     #[test]
     fn unify_type_variable_same() {
+        let mut semantic = Semantic::new();
         let pty0 = Rc::new(RefCell::new(sem::Type::TypeVariable {
             name: "a".to_string(),
             instance: None,
@@ -382,7 +378,7 @@ mod tests {
             instance: None,
         }));
 
-        unify(&pty0, &pty1);
+        semantic.unify(&pty0, &pty1);
 
         assert_matches!(*pty0.borrow(), sem::Type::TypeVariable {
             ref name,
@@ -401,6 +397,7 @@ mod tests {
     }
     #[test]
     fn unify_undetermined_type_variables() {
+        let mut semantic = Semantic::new();
         let pty0 = Rc::new(RefCell::new(sem::Type::TypeVariable {
             name: "a".to_string(),
             instance: None,
@@ -410,7 +407,7 @@ mod tests {
             instance: None,
         }));
 
-        unify(&pty0, &pty1);
+        semantic.unify(&pty0, &pty1);
 
         assert_matches!(*pty0.borrow(), sem::Type::TypeVariable {
             ref name,
@@ -438,28 +435,31 @@ mod tests {
 
     #[test]
     fn fresh_int32() {
+        let mut semantic = Semantic::new();
         let mut non_generic_vars = HashSet::new();
         let pty0 = Rc::new(RefCell::new(sem::Type::Int32));
-        let pty1 = fresh(&pty0, &mut non_generic_vars);
+        let pty1 = semantic.fresh(&pty0, &mut non_generic_vars);
 
         assert_eq!(pty0, pty1);
     }
 
     #[test]
     fn fresh_function() {
+        let mut semantic = Semantic::new();
         let mut non_generic_vars = HashSet::new();
 
         let pty0 = Rc::new(RefCell::new(sem::Type::Function {
             params: vec![],
             return_type: Rc::new(RefCell::new(sem::Type::Boolean)),
         }));
-        let pty1 = fresh(&pty0, &mut non_generic_vars);
+        let pty1 = semantic.fresh(&pty0, &mut non_generic_vars);
 
         assert_eq!(pty0, pty1);
     }
 
     #[test]
     fn fresh_typevar() {
+        let mut semantic = Semantic::new();
         let mut non_generic_vars = HashSet::new();
         let mut mappings = HashMap::new();
 
@@ -473,15 +473,15 @@ mod tests {
             instance: None,
         }));
 
-        let fresh0 = freshrec(&pty0, &mut non_generic_vars, &mut mappings);
-        let fresh1 = freshrec(&pty1, &mut non_generic_vars, &mut mappings);
+        let fresh0 = semantic.freshrec(&pty0, &mut non_generic_vars, &mut mappings);
+        let fresh1 = semantic.freshrec(&pty1, &mut non_generic_vars, &mut mappings);
 
         // should be cached
-        let cache0 = freshrec(&pty0, &mut non_generic_vars, &mut mappings);
-        let cache1 = freshrec(&pty1, &mut non_generic_vars, &mut mappings);
+        let cache0 = semantic.freshrec(&pty0, &mut non_generic_vars, &mut mappings);
+        let cache1 = semantic.freshrec(&pty1, &mut non_generic_vars, &mut mappings);
 
-        assert_eq!(pty0, fresh0);
-        assert_eq!(pty1, fresh1);
+        assert_ne!(pty0, fresh0);
+        assert_ne!(pty1, fresh1);
         assert_ne!(fresh0, fresh1);
         assert_eq!(fresh0, cache0);
         assert_eq!(fresh1, cache1);
