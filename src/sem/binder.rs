817481ee8ea7c3d43ff944fb3e1bb148401ce5ef
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-02-21 10:09:16 +0900

    Semantic analysis

---
+++ src/sem/binder.rs
@@ -0,0 +1,305 @@
+use super::wrap;
+use super::{Binding, Environment, SemanticAnalyzer, Type};
+use crate::parser;
+use parser::{Expr, Node};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// Analyze the AST and assign variable and function bindings.
+#[derive(Debug, Default)]
+pub struct Binder {}
+
+impl SemanticAnalyzer for Binder {
+    fn analyze(&mut self, module: &mut parser::Module) {
+        let env = wrap(Environment::prelude());
+        let mut env = Environment::with_parent(env);
+
+        // First, register functions defined in this module (except for `main`).
+        if let Some(ref function) = module.function {
+            env.insert(wrap(Binding::Function {
+                name: function.name.clone(),
+                r#type: Rc::clone(&function.r#type),
+            }));
+        }
+
+        let env = wrap(env);
+
+        if let Some(ref mut function) = module.function {
+            self.analyze_function(function, &env);
+        }
+        if let Some(ref mut main) = module.main {
+            self.analyze_function(main, &env);
+        }
+
+        module.env = env;
+    }
+}
+
+impl Binder {
+    pub fn new() -> Self {
+        Binder::default()
+    }
+
+    fn analyze_function(&self, function: &mut parser::Function, env: &Rc<RefCell<Environment>>) {
+        // Construct a scope that is valid when this function is called.
+        // This contains a reference to the parent scope and the arguments.
+        let mut env = Environment::with_parent(Rc::clone(env));
+
+        // Bindings for parameters
+        for binding in &function.params {
+            env.insert(Rc::clone(binding));
+        }
+
+        let env = wrap(env);
+
+        self.analyze_expr(&mut function.body, &env);
+
+        function.env = env;
+    }
+
+    fn analyze_expr(&self, node: &mut Node, env: &Rc<RefCell<Environment>>) {
+        match &mut node.expr {
+            Expr::Integer(_) => {}
+            Expr::String { .. } => {}
+            Expr::Identifier {
+                ref name,
+                ref mut binding,
+            } => {
+                match env.borrow().get(&name) {
+                    None => panic!("Undefined variable `{}`", name),
+                    Some(ref b) => match *b.borrow() {
+                        Binding::Variable { r#type: ref ty, .. } => {
+                            binding.replace(Rc::clone(&b));
+                            node.r#type = Rc::clone(ty);
+                        }
+                        _ => panic!("Invalid bound variable `{}`", name),
+                    },
+                };
+            }
+            Expr::Invocation {
+                name,
+                ref mut arguments,
+                ref mut binding,
+            } => {
+                match env.borrow().get(&name) {
+                    None => panic!("Undefined function `{}`", name),
+                    Some(ref b) => match *b.borrow() {
+                        Binding::Function {
+                            r#type: ref function_type,
+                            ..
+                        } => {
+                            match *function_type.borrow() {
+                                Type::Function {
+                                    ref params,
+                                    ref return_type,
+                                    ..
+                                } => {
+                                    if params.len() != arguments.len() {
+                                        panic!("Wrong number of arguments. The function `{}` takes {} arguments, but {} given.", name, params.len(), arguments.len());
+                                    }
+
+                                    node.r#type = Rc::clone(return_type);
+                                }
+                                ref ty => panic!("function type expected, but was {:?}", ty),
+                            };
+                            binding.replace(Rc::clone(&b));
+                        }
+                        _ => panic!("Invalid bound function `{}`", name),
+                    },
+                };
+
+                for a in arguments {
+                    self.analyze_expr(a, env);
+                }
+            }
+            // binary op
+            Expr::Add(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("+", lhs, rhs, binding, env)
+            }
+            Expr::Sub(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("-", lhs, rhs, binding, env)
+            }
+            Expr::Rem(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("%", lhs, rhs, binding, env)
+            }
+            Expr::Mul(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("*", lhs, rhs, binding, env)
+            }
+            Expr::Div(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("/", lhs, rhs, binding, env)
+            }
+            Expr::LT(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("<", lhs, rhs, binding, env)
+            }
+            Expr::GT(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op(">", lhs, rhs, binding, env)
+            }
+            Expr::LE(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("<=", lhs, rhs, binding, env)
+            }
+            Expr::GE(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op(">=", lhs, rhs, binding, env)
+            }
+            Expr::EQ(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("==", lhs, rhs, binding, env)
+            }
+            Expr::NE(ref mut lhs, ref mut rhs, ref mut binding) => {
+                self.bind_binary_op("!=", lhs, rhs, binding, env)
+            }
+            Expr::If {
+                ref mut condition,
+                ref mut then_body,
+                ref mut else_body,
+            } => {
+                self.analyze_expr(condition, env);
+                self.analyze_expr(then_body, env);
+                if let Some(ref mut else_body) = else_body {
+                    self.analyze_expr(else_body, env);
+                }
+            }
+            Expr::Case {
+                ref mut head,
+                ref mut arms,
+                ref mut else_body,
+                ..
+            } => {
+                for parser::CaseArm {
+                    ref mut pattern,
+                    ref mut condition,
+                    ref mut then_body,
+                } in arms
+                {
+                    self.analyze_expr(head, env);
+                    if let Some(ref mut else_body) = else_body {
+                        self.analyze_expr(else_body, env);
+                    }
+
+                    // Currntly, only "Variable pattern" is supported.
+                    // - A variable pattern introduces a new environment into arm body.
+                    // - The type of a this kind of pattern is always equal to the type of head.
+                    let mut arm_env = Environment::with_parent(Rc::clone(env));
+
+                    match pattern.as_mut() {
+                        parser::Pattern::Variable(ref name, ref mut binding) => {
+                            let b = wrap(Binding::Variable {
+                                name: name.clone(),
+                                r#type: Rc::clone(&head.r#type),
+                                storage: None,
+                            });
+
+                            binding.replace(Rc::clone(&b));
+                            arm_env.insert(Rc::clone(&b));
+                        }
+                    };
+
+                    let arm_env = wrap(arm_env);
+
+                    // guard
+                    if let Some(ref mut condition) = condition {
+                        self.analyze_expr(condition, &arm_env);
+                    }
+
+                    self.analyze_expr(then_body, &arm_env);
+                }
+            }
+        };
+    }
+
+    fn bind_binary_op(
+        &self,
+        operator: &str,
+        lhs: &mut Node,
+        rhs: &mut Node,
+        binding: &mut Option<Rc<RefCell<Binding>>>,
+        env: &Rc<RefCell<Environment>>,
+    ) {
+        match env.borrow().get(operator) {
+            None => panic!(
+                "Prelude not installed. Missing binary operator `{}`",
+                operator
+            ),
+            Some(ref b) => match *b.borrow() {
+                Binding::Function { .. } => binding.replace(Rc::clone(&b)),
+                _ => panic!(
+                    "Prelude not installed. Missing binary operator `{}`",
+                    operator
+                ),
+            },
+        };
+        self.analyze_expr(lhs, env);
+        self.analyze_expr(rhs, env);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_matches::assert_matches;
+
+    #[test]
+    fn param_and_variable() {
+        let mut module = parser::parse_string(
+            "
+            fun foo(i)
+                i
+            end
+            ",
+        );
+
+        analyze(&mut module);
+
+        let function = module.function.unwrap();
+        let node = function.body;
+
+        assert_matches!(node.expr, parser::Expr::Identifier { binding: Some(ref binding), .. } => {
+            assert_matches!(*binding.borrow(), Binding::Variable { r#type: ref var_type, ..} => {
+                assert_eq!(*var_type.borrow(), *node.r#type.borrow(), "variable and node has the same type");
+                assert_matches!(*function.params[0].borrow(), Binding::Variable { r#type: ref param_type, ..} => {
+                    assert_eq!(*var_type.borrow(), *param_type.borrow(), "variable and params[0] has the same type");
+                });
+            });
+        });
+    }
+
+    #[test]
+    fn case_head_and_variable_type() {
+        let mut module = parser::parse_string(
+            "
+            fun foo(i)
+                case i
+                when x if x == 5
+                    10
+                else
+                    20
+                end
+            end
+            ",
+        );
+
+        analyze(&mut module);
+
+        let function = module.function.unwrap();
+        let expr = function.body;
+
+        assert_matches!(expr.expr, Expr::Case { ref head, ref arms, ..} => {
+            assert_matches!(arms[0].pattern.as_ref(), parser::Pattern::Variable(ref _name, ref binding) => {
+                assert!(!binding.is_none());
+
+                let binding = binding.as_ref().unwrap();
+
+                assert_matches!(*function.params[0].borrow(), Binding::Variable { r#type: ref param_type, ..} => {
+                    assert_eq!(*head.r#type.borrow(), *param_type.borrow(), "head and param[0] has the same type");
+                });
+
+                assert_matches!(*binding.borrow(), Binding::Variable { ref r#type, ..} => {
+                    assert_eq!(*head.r#type.borrow(), *r#type.borrow(), "head and variable pattern has the same type");
+                });
+            });
+        });
+    }
+
+    fn analyze(module: &mut parser::Module) {
+        let mut binder = Binder::new();
+        binder.analyze(module);
+    }
+}
---
+++ src/sem/mod.rs
@@ -0,0 +1,266 @@
+pub mod binder;
+pub mod inferencer;
+use crate::asm;
+use crate::parser;
+use crate::util::wrap;
+pub use binder::Binder;
+pub use inferencer::TypeInferencer;
+use std::cell::RefCell;
+use std::collections::hash_map;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+pub trait SemanticAnalyzer {
+    fn analyze(&mut self, module: &mut parser::Module);
+}
+
+#[derive(Debug)]
+pub enum Type {
+    Int32,
+    Boolean,
+    String,
+    // Unit type. In many functional programming languages, this is
+    // written as `()`, but I am more familiar with `Void`.
+    Void,
+    Function {
+        params: Vec<Rc<RefCell<Type>>>,
+        return_type: Rc<RefCell<Type>>,
+    },
+    TypeVariable {
+        name: String,
+        instance: Option<Rc<RefCell<Type>>>,
+    },
+}
+
+/// The name of the reference in the source code,
+/// the type of the target being referenced, and
+/// the reference to the runtime storage
+#[derive(Debug, PartialEq)]
+pub enum Binding {
+    Variable {
+        name: String,
+        r#type: Rc<RefCell<Type>>,
+        storage: Option<Rc<RefCell<asm::Storage>>>,
+    },
+    Function {
+        name: String,
+        r#type: Rc<RefCell<Type>>,
+    },
+}
+
+#[derive(Debug, Default)]
+pub struct Environment {
+    pub parent: Option<Rc<RefCell<Environment>>>,
+    bindings: HashMap<String, Rc<RefCell<Binding>>>,
+}
+
+impl Environment {
+    pub fn prelude() -> Environment {
+        let mut env = Environment::new();
+
+        // Binary operators
+        for op in &["+", "-", "%", "*", "/"] {
+            env.insert(wrap(Binding::Function {
+                name: op.to_string(),
+                r#type: wrap(Type::Function {
+                    params: vec![wrap(Type::Int32), wrap(Type::Int32)],
+                    return_type: wrap(Type::Int32),
+                }),
+            }));
+        }
+        for op in &["<", ">", "<=", ">=", "==", "!="] {
+            env.insert(wrap(Binding::Function {
+                name: op.to_string(),
+                r#type: wrap(Type::Function {
+                    params: vec![wrap(Type::Int32), wrap(Type::Int32)],
+                    return_type: wrap(Type::Boolean),
+                }),
+            }));
+        }
+        // print
+        env.insert(wrap(Binding::Function {
+            name: "println_str".to_string(),
+            r#type: wrap(Type::Function {
+                params: vec![wrap(Type::String)],
+                return_type: wrap(Type::Int32),
+            }),
+        }));
+        env.insert(wrap(Binding::Function {
+            name: "println_i32".to_string(),
+            r#type: wrap(Type::Function {
+                params: vec![wrap(Type::Int32)],
+                return_type: wrap(Type::Int32),
+            }),
+        }));
+
+        env
+    }
+
+    pub fn new() -> Self {
+        Environment::default()
+    }
+
+    pub fn with_parent(parent: Rc<RefCell<Environment>>) -> Environment {
+        Environment {
+            parent: Some(parent),
+            bindings: HashMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, binding: Rc<RefCell<Binding>>) {
+        let name = match *binding.borrow() {
+            Binding::Variable { ref name, .. } | Binding::Function { ref name, .. } => name.clone(),
+        };
+
+        self.bindings.insert(name, binding);
+    }
+
+    pub fn get(&self, name: &str) -> Option<Rc<RefCell<Binding>>> {
+        match self.bindings.get(name) {
+            None => match self.parent {
+                None => None,
+                Some(ref parent) => parent.borrow().get(name).map(|x| Rc::clone(&x)),
+            },
+            Some(binding) => Some(Rc::clone(binding)),
+        }
+    }
+
+    pub fn bindings(&self) -> hash_map::Values<String, Rc<RefCell<Binding>>> {
+        self.bindings.values()
+    }
+}
+
+impl Type {
+    pub fn new_type_var(name: &str) -> Self {
+        Type::TypeVariable {
+            name: name.to_string(),
+            instance: None,
+        }
+    }
+
+    /// Returns `true` if the type given by the 2nd argument appears in this type.
+    pub fn contains(&self, other: &Self) -> bool {
+        match self {
+            Type::Int32 => matches!(other, Type::Int32),
+            Type::Boolean => matches!(other, Type::Boolean),
+            Type::String => matches!(other, Type::String),
+            Type::Void => matches!(other, Type::Void),
+            Type::Function {
+                params,
+                return_type,
+            } => {
+                params.iter().any(|x| x.borrow().contains(other))
+                    || return_type.borrow().contains(other)
+            }
+            Type::TypeVariable { instance: None, .. } => self == other,
+            Type::TypeVariable {
+                instance: Some(instance),
+                ..
+            } => (self == other) || (**instance).borrow().contains(other),
+        }
+    }
+}
+
+impl PartialEq for Type {
+    fn eq(&self, other: &Self) -> bool {
+        match self {
+            Type::Int32 => matches!(other, Type::Int32),
+            Type::Boolean => matches!(other, Type::Boolean),
+            Type::String => matches!(other, Type::String),
+            Type::Void => matches!(other, Type::Void),
+            Type::Function {
+                params: params1,
+                return_type: return_type1,
+            } => match other {
+                Type::Function {
+                    params: params2,
+                    return_type: return_type2,
+                } => params1 == params2 && return_type1 == return_type2,
+                _ => false,
+            },
+            // TypeVariable equality depends on name only.
+            Type::TypeVariable { name: name1, .. } => match other {
+                Type::TypeVariable { name: name2, .. } => name1 == name2,
+                _ => false,
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod type_tests {
+    use super::*;
+
+    #[test]
+    fn eq_type_variable1() {
+        let ty1 = Type::TypeVariable {
+            name: "a".to_string(),
+            instance: None,
+        };
+        let ty2 = Type::TypeVariable {
+            name: "a".to_string(),
+            instance: None,
+        };
+
+        assert_eq!(ty1, ty2);
+    }
+
+    #[test]
+    fn eq_type_variable2() {
+        // TypeVariable equality depends on name only.
+        let ty1 = Type::TypeVariable {
+            name: "a".to_string(),
+            instance: None,
+        };
+        let ty2 = Type::TypeVariable {
+            name: "a".to_string(),
+            instance: Some(Rc::new(RefCell::new(Type::Int32))),
+        };
+
+        assert_eq!(ty1, ty2);
+    }
+
+    #[test]
+    fn contains_monotype() {
+        assert!(Type::Int32.contains(&Type::Int32));
+        assert!(Type::Boolean.contains(&Type::Boolean));
+        assert!(Type::String.contains(&Type::String));
+        assert!(!Type::Int32.contains(&Type::Boolean));
+    }
+    #[test]
+    fn contains_polytype() {
+        let ty1 = Type::Function {
+            params: vec![Rc::new(RefCell::new(Type::Int32))],
+            return_type: Rc::new(RefCell::new(Type::TypeVariable {
+                name: "a".to_string(),
+                instance: None,
+            })),
+        };
+
+        assert!(ty1.contains(&Type::Int32));
+        assert!(!ty1.contains(&Type::Boolean));
+        assert!(ty1.contains(&Type::TypeVariable {
+            name: "a".to_string(),
+            instance: None,
+        }));
+    }
+
+    #[test]
+    fn contains_type_variable() {
+        let ty1 = Type::Function {
+            params: vec![Rc::new(RefCell::new(Type::Int32))],
+            return_type: Rc::new(RefCell::new(Type::TypeVariable {
+                name: "a".to_string(),
+                instance: Some(Rc::new(RefCell::new(Type::Boolean))),
+            })),
+        };
+
+        assert!(!ty1.contains(&Type::String));
+        assert!(ty1.contains(&Type::Int32));
+        assert!(ty1.contains(&Type::Boolean));
+        assert!(ty1.contains(&Type::TypeVariable {
+            name: "a".to_string(),
+            instance: None,
+        }));
+    }
+}
