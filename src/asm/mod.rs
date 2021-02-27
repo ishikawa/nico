a0094f31fa904d3ddc2afa3801cbfc979faaddd8
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-02-28 07:53:24 +0900

    memory layout

--- src/asm/mod.rs
+++ src/asm/mod.rs
@@ -1,3 +1,48 @@
+//! ## Memory Layout
+//!
+//! ```
+//!    +-------+-----+-------------------+---------------+----------+
+//!    |       | ... | Caller's FP (i32) | Constant Pool | Heap ... |
+//!    o-------o-----o-------------------o---------------o----------+
+//!    |       |     |                   |               |          
+//! index 0   SP    FP              Stack Offset     Heap Offset
+//! ```
+//!
+//! ### Global Context Registers
+//!
+//! Global variables that are shared by multiple execution contexts.
+//!
+//! - **Stack Pointer (SP)** - Stack pointer. End of stack.
+//! - **Stack Offset** - Start index of the virtual stack segment. The virtual stack extends in
+//!   the negative direction. If the access is out of range, it will be trapped in the host environment.
+//! - **Heap Offset** - Start index of the heap segment. The heap extends in the positive direction.
+//!   If the access is out of range, it will be trapped in the host environment.
+//!
+//! ### Execution Context Registers
+//!
+//! Run-time context allocated in global variables. We should be able to switch between multiple
+//! execution contexts by saving them somewhere.
+//!
+//! - **Frame Pointer (FP)** - The frame pointer of the current function.
+//!
+//! ### Virtual Stack Segment
+//!
+//! Unlike WebAssembly's value stack, it is placed in a contiguous memory area and a frame is
+//! allocated for each function call.
+//!
+//! - When the closure is introduced, we will secure not only the FP of the caller,
+//!   but also the FP of the function where a closure is defined.
+//! - Can it be laid out well so that variables that are not referenced by the closure can be discarded?
+//!
+//! ### Constant Pool
+//!
+//! This is the area of the object whose size is determined at compile time and survives throughout
+//! the execution of the program, starting at Stack Offset and extending in the positive direction to Heap Offset.
+//!
+//! ### Heap
+//!
+//! A memory area for dynamically allocated objects. How to manage the memory is still under consideration.
+
 pub mod allocator;
 pub mod emitter;
 pub mod wasm;
