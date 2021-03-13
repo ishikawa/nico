1dfe05a2608c92bd839349811252a737b2c79ffd
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-03-14 03:58:54 +0900

    add test

--- __tests__/runner.test.ts
+++ __tests__/runner.test.ts
@@ -15,32 +15,72 @@ test("given empty input", async () => {
   consoleSpy.mockReset();
 });
 
-test("fizzbuzz", async () => {
-  const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
-  const outputFilepath = path.join(os.tmpdir(), "fizzbuzz.wat");
+interface TestCase {
+  file: string;
+  consoleResult: string[][];
+}
 
-  await compileFileToWATFile("./samples/fizzbuzz.nico", outputFilepath);
+const cases: TestCase[] = [
+  {
+    file: "./samples/fib.nico",
+    consoleResult: [
+      ["0"],
+      ["1"],
+      ["1"],
+      ["2"],
+      ["3"],
+      ["5"],
+      ["8"],
+      ["13"],
+      ["21"],
+      ["34"],
+      ["55"],
+      ["89"],
+      ["144"],
+      ["233"],
+      ["377"],
+      ["610"],
+      ["987"],
+      ["1597"],
+      ["2584"],
+      ["4181"],
+      ["6765"]
+    ]
+  },
+  {
+    file: "./samples/fizzbuzz.nico",
+    consoleResult: [
+      ["1"],
+      ["2"],
+      ["Fizz"],
+      ["4"],
+      ["Buzz"],
+      ["Fizz"],
+      ["7"],
+      ["8"],
+      ["Fizz"],
+      ["Buzz"],
+      ["11"],
+      ["Fizz"],
+      ["13"],
+      ["14"],
+      ["Fizz Buzz"]
+    ]
+  }
+];
 
-  let code = await main(["node", "nico", outputFilepath]);
+cases.forEach(({ file, consoleResult }) => {
+  test(file, async () => {
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+    const outputFilepath = path.join(os.tmpdir(), path.basename(file));
 
-  expect(code).toEqual(0);
-  expect(consoleSpy.mock.calls).toEqual([
-    ["1"],
-    ["2"],
-    ["Fizz"],
-    ["4"],
-    ["Buzz"],
-    ["Fizz"],
-    ["7"],
-    ["8"],
-    ["Fizz"],
-    ["Buzz"],
-    ["11"],
-    ["Fizz"],
-    ["13"],
-    ["14"],
-    ["Fizz Buzz"]
-  ]);
+    await compileFileToWATFile(file, outputFilepath);
 
-  consoleSpy.mockReset();
+    let code = await main(["node", "nico", outputFilepath]);
+
+    expect(code).toEqual(0);
+    expect(consoleSpy.mock.calls).toEqual(consoleResult);
+
+    consoleSpy.mockReset();
+  });
 });
