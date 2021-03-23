b8fb81f094c90c081e7a366b1e7d8b9d4488c471
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-03-24 07:52:22 +0900

    naive implementation

--- src/bin/nico-ls.rs
+++ src/bin/nico-ls.rs
@@ -1,138 +1,77 @@
-use log::info;
+use log::{info, warn};
 use serde_json::Value;
-use std::net::Ipv4Addr;
-use tokio::net::TcpListener;
-use tower_lsp::jsonrpc::Result;
-use tower_lsp::lsp_types::*;
-use tower_lsp::{Client, LanguageServer, LspService, Server};
+use std::io;
+use std::io::Read;
 
-/// TCP port for LSP
-const RPC_PORT: u16 = 9257;
-#[derive(Debug)]
-struct Backend {
-    client: Client,
-}
-
-#[tower_lsp::async_trait]
-impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult {
-            server_info: None,
-            capabilities: ServerCapabilities {
-                text_document_sync: Some(TextDocumentSyncCapability::Kind(
-                    TextDocumentSyncKind::Incremental,
-                )),
-                completion_provider: Some(CompletionOptions {
-                    resolve_provider: Some(false),
-                    trigger_characters: Some(vec![".".to_string()]),
-                    work_done_progress_options: Default::default(),
-                }),
-                execute_command_provider: Some(ExecuteCommandOptions {
-                    commands: vec!["dummy.do_something".to_string()],
-                    work_done_progress_options: Default::default(),
-                }),
-                workspace: Some(WorkspaceCapability {
-                    workspace_folders: Some(WorkspaceFolderCapability {
-                        supported: Some(true),
-                        change_notifications: Some(
-                            WorkspaceFolderCapabilityChangeNotifications::Bool(true),
-                        ),
-                    }),
-                }),
-                ..ServerCapabilities::default()
-            },
-        })
-    }
-
-    async fn initialized(&self, _: InitializedParams) {
-        self.client
-            .log_message(MessageType::Info, "initialized!")
-            .await;
-    }
-
-    async fn shutdown(&self) -> Result<()> {
-        Ok(())
-    }
-
-    async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
-        self.client
-            .log_message(MessageType::Info, "workspace folders changed!")
-            .await;
-    }
-
-    async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
-        self.client
-            .log_message(MessageType::Info, "configuration changed!")
-            .await;
-    }
-
-    async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
-        self.client
-            .log_message(MessageType::Info, "watched files have changed!")
-            .await;
-    }
-
-    async fn execute_command(&self, _: ExecuteCommandParams) -> Result<Option<Value>> {
-        self.client
-            .log_message(MessageType::Info, "command executed!")
-            .await;
-
-        match self.client.apply_edit(WorkspaceEdit::default()).await {
-            Ok(res) if res.applied => self.client.log_message(MessageType::Info, "applied").await,
-            Ok(_) => self.client.log_message(MessageType::Info, "rejected").await,
-            Err(err) => self.client.log_message(MessageType::Error, err).await,
-        }
-
-        Ok(None)
-    }
-
-    async fn did_open(&self, _: DidOpenTextDocumentParams) {
-        self.client
-            .log_message(MessageType::Info, "file opened!")
-            .await;
-    }
-
-    async fn did_change(&self, _: DidChangeTextDocumentParams) {
-        self.client
-            .log_message(MessageType::Info, "file changed!")
-            .await;
-    }
-
-    async fn did_save(&self, _: DidSaveTextDocumentParams) {
-        self.client
-            .log_message(MessageType::Info, "file saved!")
-            .await;
-    }
-
-    async fn did_close(&self, _: DidCloseTextDocumentParams) {
-        self.client
-            .log_message(MessageType::Info, "file closed!")
-            .await;
-    }
-
-    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
-        Ok(Some(CompletionResponse::Array(vec![
-            CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
-            CompletionItem::new_simple("Bye".to_string(), "More detail".to_string()),
-        ])))
-    }
-}
-
-#[tokio::main]
-async fn main() {
+fn main() {
     env_logger::init();
+
     info!("Launching language server...");
 
-    let addr = (Ipv4Addr::LOCALHOST, RPC_PORT);
-    let mut listener = TcpListener::bind(addr).await.unwrap();
-    info!("Listening on {}:{}", addr.0, addr.1);
+    loop {
+        let mut content_length: Option<usize> = None;
 
-    let (stream, _) = listener.accept().await.unwrap();
-    let (read, write) = tokio::io::split(stream);
+        // Header Part
+        // https://microsoft.github.io/language-server-protocol/specifications/specification-current/#headerPart
+        //
+        // HTTP header field
+        // -----------------
+        // Each header field consists of a case-insensitive field name followed by a colon (":"), optional leading
+        // whitespace, the field value, and optional trailing whitespace.
+        // https://tools.ietf.org/html/rfc7230#section-3.2
+        loop {
+            let mut header = String::new();
 
-    let (service, messages) = LspService::new(|client| Backend { client });
-    Server::new(read, write)
-        .interleave(messages)
-        .serve(service)
-        .await;
+            io::stdin()
+                .read_line(&mut header)
+                .expect("Failed to read line");
+
+            info!("line: {:?}", header);
+
+            if header == "\r\n" {
+                // ‘\r\n’ sequences always immediately precede the content part of a message.
+                break;
+            }
+
+            let name_value = header.splitn(2, ':').collect::<Vec<_>>();
+
+            if name_value.len() != 2 {
+                warn!("Received illegal formatted header: {}", header);
+                continue;
+            }
+
+            let name = name_value[0].trim().to_ascii_lowercase();
+            let value = name_value[1].trim().to_ascii_lowercase();
+
+            if name == "content-length" {
+                match value.parse::<usize>() {
+                    Ok(length) => {
+                        content_length = Some(length);
+                    }
+                    Err(e) => {
+                        warn!("Received invalid content-length : {} - {}", value, e);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        // Content Part
+        if let Some(content_length) = content_length {
+            let m: Vec<u8> = io::stdin()
+                .bytes()
+                .take(content_length)
+                .map(|r: Result<u8, _>| r.unwrap())
+                .collect();
+
+            match serde_json::from_slice::<Value>(m.as_slice()) {
+                Ok(v) => {
+                    info!("[JSON] {}", v);
+                }
+                Err(e) => {
+                    warn!("Received invalid json-rpc : {}", e);
+                }
+            }
+        }
+    }
 }
