26af8fc4c5156325038d74fae9f6a67556f39066
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-03-25 07:38:44 +0900

    refactoring

--- src/bin/nico-ls.rs
+++ src/bin/nico-ls.rs
@@ -1,12 +1,12 @@
 use log::{info, warn};
 use lsp_types::{
-    ColorProviderCapability, InitializeParams, InitializeResult, ServerCapabilities, ServerInfo,
-    TextDocumentSyncCapability, TextDocumentSyncKind,
+    ColorProviderCapability, DidOpenTextDocumentParams, InitializeParams, InitializeResult,
+    ServerCapabilities, ServerInfo, TextDocumentSyncCapability, TextDocumentSyncKind,
 };
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::io;
 use std::io::{Read, Write};
+use std::{io, string};
 
 #[derive(Debug, PartialEq, Clone, Deserialize, Serialize)]
 #[serde(untagged)]
@@ -19,7 +19,7 @@ enum Id {
 #[derive(Debug, PartialEq, Clone, Deserialize, Serialize)]
 struct Request {
     jsonrpc: String,
-    id: Id,
+    id: Option<Id>,
     method: String,
     params: Option<Value>,
 }
@@ -39,143 +39,176 @@ struct ResponseError {
     data: Option<Value>,
 }
 
+#[derive(Debug)]
+struct HandlerError {
+    kind: HandlerErrorKind,
+}
+
+#[derive(Debug)]
+enum HandlerErrorKind {
+    // Errors
+    UTF8Error(string::FromUtf8Error),
+    IOError(io::Error),
+    JSONError(serde_json::Error),
+
+    // Warnings
+    InvalidParams,
+}
+
+impl From<io::Error> for HandlerError {
+    fn from(err: io::Error) -> Self {
+        Self {
+            kind: HandlerErrorKind::IOError(err),
+        }
+    }
+}
+
+impl From<string::FromUtf8Error> for HandlerError {
+    fn from(err: string::FromUtf8Error) -> Self {
+        Self {
+            kind: HandlerErrorKind::UTF8Error(err),
+        }
+    }
+}
+
+impl From<serde_json::Error> for HandlerError {
+    fn from(err: serde_json::Error) -> Self {
+        Self {
+            kind: HandlerErrorKind::JSONError(err),
+        }
+    }
+}
+
+impl Request {
+    fn take_params<T: serde::de::DeserializeOwned>(&mut self) -> Result<T, HandlerError> {
+        match self.params.take() {
+            None => Err(HandlerError {
+                kind: HandlerErrorKind::InvalidParams,
+            }),
+            Some(params) => Ok(serde_json::from_value::<T>(params)?),
+        }
+    }
+}
+
+impl Response {
+    fn from_value(request: &Request, value: Value) -> Self {
+        Self {
+            jsonrpc: request.jsonrpc.clone(),
+            id: request.id.clone().unwrap_or_else(|| Id::Int(-1)),
+            result: Some(value),
+            error: None,
+        }
+    }
+}
+
+fn event_loop_main() -> Result<(), HandlerError> {
+    let mut content_length: Option<usize> = None;
+
+    // Header Part
+    // https://microsoft.github.io/language-server-protocol/specifications/specification-current/#headerPart
+    //
+    // HTTP header field
+    // -----------------
+    // Each header field consists of a case-insensitive field name followed by a colon (":"), optional leading
+    // whitespace, the field value, and optional trailing whitespace.
+    // https://tools.ietf.org/html/rfc7230#section-3.2
+    loop {
+        let mut header = String::new();
+
+        io::stdin().read_line(&mut header)?;
+
+        info!("line: {:?}", header);
+
+        if header == "\r\n" {
+            // ‘\r\n’ sequences always immediately precede the content part of a message.
+            break;
+        }
+
+        let name_value = header.splitn(2, ':').collect::<Vec<_>>();
+
+        if name_value.len() != 2 {
+            warn!("Received illegal formatted header: {}", header);
+            todo!("Error notification and return");
+        }
+
+        let name = name_value[0].trim().to_ascii_lowercase();
+        let value = name_value[1].trim().to_ascii_lowercase();
+
+        if name == "content-length" {
+            match value.parse::<usize>() {
+                Ok(length) => {
+                    content_length = Some(length);
+                }
+                Err(e) => {
+                    warn!("Received invalid content-length : {} - {}", value, e);
+                    todo!("Error notification and return");
+                }
+            }
+        }
+    }
+
+    // Content Part
+    let content_length = content_length.expect("Content-Length is mandatory");
+
+    let bytes: Vec<u8> = io::stdin()
+        .bytes()
+        .take(content_length)
+        .map(|r: Result<u8, _>| r.unwrap())
+        .collect();
+
+    let string = String::from_utf8(bytes)?;
+    let value = serde_json::from_str::<Value>(&string)?;
+    let mut request = serde_json::from_value::<Request>(value)?;
+
+    match request.method.as_str() {
+        // Requests
+        "initialize" => {
+            let params = request.take_params::<InitializeParams>()?;
+            info!("[initialize] {:?}", params);
+
+            let result = InitializeResult {
+                capabilities: ServerCapabilities {
+                    text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                        TextDocumentSyncKind::Full,
+                    )),
+
+                    color_provider: Some(ColorProviderCapability::Simple(true)),
+                    ..ServerCapabilities::default()
+                },
+                server_info: Some(ServerInfo {
+                    name: "nico-ls".to_string(),
+                    version: Some("0.0.1".to_string()),
+                }),
+            };
+
+            let value = serde_json::to_value(result)?;
+            let response = Response::from_value(&request, value);
+            let json = serde_json::to_vec(&response)?;
+
+            write!(io::stdout(), "Content-Length: {}\r\n\r\n", json.len())?;
+            io::stdout().write_all(json.as_slice())?;
+            io::stdout().flush()?;
+        }
+        // Notifications
+        "textDocument/didOpen" => {
+            let params = request.take_params::<DidOpenTextDocumentParams>()?;
+            info!("[textDocument/didOpen] {:?}", params);
+        }
+        _ => {
+            warn!("[unknown] {:?}", request);
+        }
+    };
+
+    Ok(())
+}
+
 fn main() {
     env_logger::init();
 
     info!("Launching language server...");
-
     loop {
-        let mut content_length: Option<usize> = None;
-
-        // Header Part
-        // https://microsoft.github.io/language-server-protocol/specifications/specification-current/#headerPart
-        //
-        // HTTP header field
-        // -----------------
-        // Each header field consists of a case-insensitive field name followed by a colon (":"), optional leading
-        // whitespace, the field value, and optional trailing whitespace.
-        // https://tools.ietf.org/html/rfc7230#section-3.2
-        loop {
-            let mut header = String::new();
-
-            io::stdin()
-                .read_line(&mut header)
-                .expect("Failed to read line");
-
-            info!("line: {:?}", header);
-
-            if header == "\r\n" {
-                // ‘\r\n’ sequences always immediately precede the content part of a message.
-                break;
-            }
-
-            let name_value = header.splitn(2, ':').collect::<Vec<_>>();
-
-            if name_value.len() != 2 {
-                warn!("Received illegal formatted header: {}", header);
-                continue;
-            }
-
-            let name = name_value[0].trim().to_ascii_lowercase();
-            let value = name_value[1].trim().to_ascii_lowercase();
-
-            if name == "content-length" {
-                match value.parse::<usize>() {
-                    Ok(length) => {
-                        content_length = Some(length);
-                    }
-                    Err(e) => {
-                        warn!("Received invalid content-length : {} - {}", value, e);
-                        continue;
-                    }
-                }
-            }
-        }
-
-        // Content Part
-        if let Some(content_length) = content_length {
-            let bytes: Vec<u8> = io::stdin()
-                .bytes()
-                .take(content_length)
-                .map(|r: Result<u8, _>| r.unwrap())
-                .collect();
-
-            let string = match String::from_utf8(bytes) {
-                Ok(s) => s,
-                Err(e) => {
-                    warn!(
-                        "Couldn't convert {} bytes into string : {}",
-                        content_length, e
-                    );
-                    continue;
-                }
-            };
-
-            let value = match serde_json::from_str::<Value>(&string) {
-                Ok(value) => value,
-                Err(e) => {
-                    warn!("JSON parse error : {} - {}", e, &string);
-                    continue;
-                }
-            };
-
-            let request = match serde_json::from_value::<Request>(value) {
-                Ok(request) => request,
-                Err(e) => {
-                    warn!("JSON-RPC parse error : {} - {}", e, &string);
-                    continue;
-                }
-            };
-
-            match request.method.as_str() {
-                "initialize" => {
-                    match serde_json::from_value::<InitializeParams>(request.params.unwrap()) {
-                        Ok(params) => {
-                            info!("[initialize] {:?}", params);
-
-                            let result = InitializeResult {
-                                capabilities: ServerCapabilities {
-                                    text_document_sync: Some(TextDocumentSyncCapability::Kind(
-                                        TextDocumentSyncKind::Full,
-                                    )),
-
-                                    color_provider: Some(ColorProviderCapability::Simple(true)),
-                                    ..ServerCapabilities::default()
-                                },
-                                server_info: Some(ServerInfo {
-                                    name: "nico-ls".to_string(),
-                                    version: Some("0.0.1".to_string()),
-                                }),
-                            };
-
-                            let value = serde_json::to_value(result).unwrap();
-
-                            let response = Response {
-                                jsonrpc: request.jsonrpc,
-                                id: request.id,
-                                result: Some(value),
-                                error: None,
-                            };
-
-                            let json = serde_json::to_vec(&response).expect("json error");
-
-                            info!("[write] {:?}", &response);
-                            write!(io::stdout(), "Content-Length: {}\r\n\r\n", json.len())
-                                .expect("write error");
-                            io::stdout()
-                                .write_all(json.as_slice())
-                                .expect("write error");
-                            io::stdout().flush().expect("write error");
-                        }
-                        Err(e) => {
-                            warn!("initialize: parse error : {} - {}", e, &string);
-                        }
-                    };
-                }
-                _ => {
-                    warn!("[unknown] {:?}", request);
-                }
-            };
+        if let Err(err) = event_loop_main() {
+            todo!("Handle error or send notification to the client: {:?}", err)
         }
     }
 }
