c8029b3afd43d5bba896c2eeaec6cf75f4673106
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-03-29 08:40:52 +0900

    SyntaxTokens

--- src/syntax/tree.rs
+++ src/syntax/tree.rs
@@ -1,13 +1,14 @@
 use crate::sem;
 use crate::tokenizer::Token;
-use std::cell::RefCell;
 use std::rc::Rc;
+use std::{cell::RefCell, slice};
 
 #[derive(Debug)]
 pub enum SyntaxToken {
     Interpreted(Rc<Token>),
     Missing(Rc<Token>),
     Skipped(Rc<Token>),
+    Child,
 }
 
 #[derive(Debug)]
@@ -101,19 +102,86 @@ pub struct ExprNode {
 #[derive(Debug)]
 pub enum Expr {
     Integer(i32),
+
+    // Binary Op
+    // tokens: [...lhs, <operator>, ...rhs]
+    Add(
+        Box<ExprNode>,
+        Box<ExprNode>,
+        Option<Rc<RefCell<sem::Binding>>>,
+    ),
 }
+
+type SyntaxTokenIterator<'a> = Box<dyn Iterator<Item = &'a SyntaxToken> + 'a>;
 
 // --- tokens
 impl StatementNode {
-    pub fn tokens(&self) -> Box<dyn Iterator<Item = &'_ SyntaxToken> + '_> {
+    pub fn tokens(&self) -> SyntaxTokenIterator<'_> {
         self.expr.tokens()
     }
 }
 
 impl ExprNode {
-    pub fn tokens(&self) -> Box<dyn Iterator<Item = &'_ SyntaxToken> + '_> {
+    pub fn tokens(&self) -> SyntaxTokenIterator<'_> {
         match self.kind {
             Expr::Integer(_) => Box::new(self.code.tokens.iter()),
+            Expr::Add(ref lhs, ref rhs, ..) => Box::new(SyntaxTokens::new(
+                self.code.tokens.iter(),
+                vec![lhs.tokens(), rhs.tokens()],
+            )),
         }
+    }
+}
+
+pub struct SyntaxTokens<'a> {
+    iterator: slice::Iter<'a, SyntaxToken>,
+    children: Vec<SyntaxTokenIterator<'a>>,
+    child_iterator: Option<SyntaxTokenIterator<'a>>,
+    child_index: usize,
+}
+
+impl<'a> SyntaxTokens<'a> {
+    pub fn new(
+        iterator: slice::Iter<'a, SyntaxToken>,
+        children: Vec<SyntaxTokenIterator<'a>>,
+    ) -> Self {
+        Self {
+            iterator,
+            children,
+            child_iterator: None,
+            child_index: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for SyntaxTokens<'a> {
+    type Item = &'a SyntaxToken;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(ref mut child_iterator) = self.child_iterator {
+            let token = child_iterator.next();
+
+            if token.is_none() {
+                self.child_iterator = None;
+            } else {
+                return token;
+            }
+        }
+
+        let next = self.iterator.next();
+
+        if let Some(SyntaxToken::Child) = next {
+            if self.child_index >= self.children.len() {
+                panic!("children vec overflow: {}", self.children.len());
+            }
+
+            let mut it = self.children.remove(self.child_index);
+            let token = it.next();
+
+            self.child_iterator.replace(it);
+            return token;
+        }
+
+        next
     }
 }
