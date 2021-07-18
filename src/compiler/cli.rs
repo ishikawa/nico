e10c01b73d69e57ece5adec5c05e90abc054cc73
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-07-18 16:00:44 +0900

    backend option

--- src/compiler/cli.rs
+++ src/compiler/cli.rs
@@ -1,9 +1,3 @@
-use crate::asm::wasm::Printer as WasmPrinter;
-use crate::asm::AsmBuilder;
-use crate::compiler::CompilerError;
-use crate::parser::Parser;
-use crate::syntax::Tokenizer;
-use crate::CompilerPasses;
 use clap::{App, Arg};
 use io::Read;
 use std::array;
@@ -12,6 +6,10 @@ use std::fs;
 use std::io;
 use std::iter::ExactSizeIterator;
 use std::str::FromStr;
+
+use super::backend_2021_spring;
+use super::backend_2021_summer;
+use super::CompilerError;
 
 #[derive(Debug, Clone, Copy)]
 pub enum CompilerBackend {
@@ -85,21 +83,10 @@ impl Command {
             read_from_stdin()?
         };
 
-        let mut tokenizer = Tokenizer::from_string(&src);
-        let parser = Parser::new();
-        let mut module = parser.parse(&mut tokenizer)?;
-
-        let mut passes = CompilerPasses::new();
-        passes.apply(&mut module);
-
-        let builder = AsmBuilder::new();
-        let mut printer = WasmPrinter::new();
-
-        let wasm_module = builder.build_module(&module);
-
-        printer.pretty = true;
-        printer.write_module(&wasm_module);
-        Ok(printer.to_string())
+        match options.backend {
+            CompilerBackend::Nico2021Spring => backend_2021_spring::compile(src),
+            CompilerBackend::Nico2021Summer => backend_2021_summer::compile(src),
+        }
     }
 }
 
